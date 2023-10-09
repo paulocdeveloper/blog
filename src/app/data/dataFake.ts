c36a24fc9690ea18b7c4fbd35b0ac4e4776c0952
paulocdeveloper <gow.luukz122@gmail.com>
2023-10-08 22:45:46 -0300

    conted created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"Oque esse burro fez vai te chocar!",
+        "description":"Aqui vai ficar a descrição de seu artigo!",
+        "photoCover":"https://agron.com.br/wp-content/uploads/2021/06/01-16.jpg"
+
+    },
+    {
+        "id":"2",
+        "title":"Nova vaquinha chega ao campo!",
+        "description":"Aqui vai ficar a descrição de seu artigo!",
+        "photoCover":"https://clubedogado.com.br/wp-content/uploads/2023/03/qual-o-melhor-capim-para-vaca-leiteira-01.jpg"
+    },
+    {
+        "id":"3",
+        "title":"Porquinho da fazenda ganha o coração do povo",
+        "description":"Aqui vai ficar a descrição de seu artigo!",
+        "photoCover":"https://img.olhardigital.com.br/wp-content/uploads/2022/03/grunhidos-porcos-capa.jpg"
+    },
+    {
+        "id":"4",
+        "title":"Ovelhas fofinhas",
+        "description":"Aqui vai ficar a descrição de seu artigo!",
+        "photoCover":"https://conteudo.imguol.com.br/c/bol/fotos/db/2018/02/15/ovelha-faz-pose-carneiro-femea-carneira-borrega-cordeira-animal-de-fazenda-1518694334742_1024x768.jpg"
+    }
+]
